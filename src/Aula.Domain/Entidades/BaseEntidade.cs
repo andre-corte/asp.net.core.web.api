a68a06edf7a944b9182230c65e316ecba16f78bf
André Côrte <suporte@fidelizarmais.com.br>
2021-02-13 15:08:48 -0300

    Criado entidade e table

---
+++ src/Aula.Domain/Entidades/BaseEntidade.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Aula.Domain.Entidades
+{
+    public abstract class BaseEntidade
+    {
+        public long Id { get; set; }
+        public DateTime DataCadastro { get; set; }
+        public string ResponsavelCadastro { get; set; }
+        public bool Ativo { get; set; }
+    }
+}
---
+++ src/Aula.Domain/Validation/UsuarioValidation.cs
@@ -0,0 +1,43 @@
+﻿using Aula.Domain.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Aula.Domain.Validation
+{
+    public static class UsuarioValidation
+    {
+        public static bool Validar(Usuario usuario)
+        {
+            List<string> vs = new List<string>(0);
+
+            if (string.IsNullOrWhiteSpace(usuario.ResponsavelCadastro))
+            {
+                vs.Add("Responsável cadastro");
+            }
+
+            if (string.IsNullOrWhiteSpace(usuario.Email))
+            {
+                vs.Add("E-mail");
+            }
+
+            if (string.IsNullOrWhiteSpace(usuario.Senha))
+            {
+                vs.Add("Senha");
+            }
+
+            if (usuario.DataCadastro == default)
+            {
+                vs.Add("Data cadastro");
+            }
+
+            if (vs.Any())
+            {
+                throw new Exception($"Informe os campos abaixo: {Environment.NewLine}{string.Join(", ", vs)}");
+            }
+
+            return true;
+        }
+    }
+}
